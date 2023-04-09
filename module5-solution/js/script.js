47dba3aefdebe014b60c7f8297f451113122e42a
stev-app <126858050+stev-app@users.noreply.github.com>
2023-04-09 12:07:11 +0800

    fix div id

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -130,7 +130,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHtml("#main-content",homeHtmlToInsertIntoMainPage)
+      insertHtml("#specials-tile",homeHtmlToInsertIntoMainPage)
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
